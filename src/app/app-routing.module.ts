9b554a8d4967cd98e25122e30cf22f6859ae29b3
Eric Scott <escott@archer.local>
2020-08-06 18:23:43 -0600

    inital

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "profile",
+  component: ProfileComponent
+},
+{
+  path: "login",
+  component: AuthComponent
+},
+{
+  path: '**',
+  redirectTo: 'login',
+  pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
