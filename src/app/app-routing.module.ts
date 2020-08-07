b6a2a05fb86edbaf8fd87754ab51bce7fb47ac65
Eric Scott <escott@archer.local>
2020-08-06 18:29:33 -0600

    updated bad app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [{
   path: "profile",
